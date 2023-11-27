b145fbd1c81dd5b8f4465ba0a2087929ad1780c6
Adil S <forever@nzironeed.ru>
2023-11-27 13:24:12 +0500

    add user model

--- library_manager/users/models.py
+++ library_manager/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True, verbose_name='user_name')
+    email = models.EmailField(max_length=150, unique=True, verbose_name='user_email')
+    registration_date = models.DateField(auto_now_add=True, verbose_name='user_registration_date')
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f'{self.email}; {self.registration_date}'
