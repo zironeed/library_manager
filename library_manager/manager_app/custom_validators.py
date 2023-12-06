05cc91ed151d78c709ea49b630ce8aca187f34de
Adil S <forever@nzironeed.ru>
2023-12-06 22:43:08 +0500

    add docstrings and comments

--- library_manager/manager_app/custom_validators.py
+++ library_manager/manager_app/custom_validators.py
@@ -2,10 +2,16 @@ from rest_framework import validators
 
 
 def publish_year_validator(year):
+    """
+    Валидация года выпуска книги
+    """
     if year > 2023 or year < 1800:
         raise validators.ValidationError('Enter the correct publication year')
 
 
 def isbn_validator(isbn):
+    """
+    Валидация ISBN-номера книги
+    """
     if len(str(isbn)) != 10 and len(str(isbn)) != 13:
         raise validators.ValidationError('Enter the correct ISBN')
--- library_manager/users/tasks.py
+++ library_manager/users/tasks.py
@@ -4,4 +4,7 @@ from users.services import send_mail
 
 @shared_task
 def send_letter(email):
+    """
+    Отправка приветственного письма пользователю
+    """
     send_mail(email)
